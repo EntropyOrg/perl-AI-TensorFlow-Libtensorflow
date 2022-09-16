7acd721077c29b19f0a39d3446f6be632b8ab90d
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-09-16 11:09:10 -0400

    Add more references to C function name

--- lib/AI/TensorFlow/Libtensorflow/Session.pm
+++ lib/AI/TensorFlow/Libtensorflow/Session.pm
@@ -4,12 +4,14 @@ use AI::TensorFlow::Libtensorflow;
 my $ffi = AI::TensorFlow::Libtensorflow::Lib->ffi;
 $ffi->mangler(AI::TensorFlow::Libtensorflow::Lib->mangler_default);
 
+# C: TF_NewSession
 $ffi->attach( [ 'NewSession' => '_New' ] =>
 	[ 'TF_Graph', 'TF_SessionOptions', 'TF_Status' ],
 	=> 'TF_Session' => sub {
 		my ($xs, $class, @rest) = @_;
 		return $xs->(@rest);
 	});
+# C: TF_SessionRun
 $ffi->attach( [ 'SessionRun' => 'Run' ] =>
 	[ 'TF_Session',
 	'TF_Buffer',
@@ -20,6 +22,7 @@ $ffi->attach( [ 'SessionRun' => 'Run' ] =>
 	'TF_Status',
 	],
 	=> 'void' );
+# C: TF_CloseSession
 $ffi->attach( [ 'CloseSession' => 'Close' ] =>
 	[ 'TF_Session',
 	'TF_Status',
