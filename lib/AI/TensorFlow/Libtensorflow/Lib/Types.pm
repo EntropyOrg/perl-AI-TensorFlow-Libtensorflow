3be6d4b4a376783ab74fed48403d8b4e2ce1f2ad
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-10-08 08:12:20 -0400

    Add type for ::DataType

--- lib/AI/TensorFlow/Libtensorflow/Lib/Types.pm
+++ lib/AI/TensorFlow/Libtensorflow/Lib/Types.pm
@@ -5,6 +5,7 @@ use Type::Library 0.008 -base,
 	-declare => [qw(
 		TFTensor
 		TFGraph
+		TFDataType
 	)];
 use Type::Utils -all;
 
@@ -21,5 +22,12 @@ Type for class L<AI::TensorFlow::Libtensorflow::Graph>.
 
 =cut
 class_type TFGraph => { class => 'AI::TensorFlow::Libtensorflow::Graph' };
+
+=type TFDataType
+
+Type for class L<AI::TensorFlow::Libtensorflow::DataType>
+
+=cut
+class_type TFDataType => { class => 'AI::TensorFlow::Libtensorflow::DataType' };
 
 1;
