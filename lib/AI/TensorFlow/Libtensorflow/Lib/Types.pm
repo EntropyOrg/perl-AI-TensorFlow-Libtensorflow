e3af471eeb1726c90167acfcc4700d37d7f774bb
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-10-24 14:51:43 -0400

    Add Dims type

--- lib/AI/TensorFlow/Libtensorflow/Lib/Types.pm
+++ lib/AI/TensorFlow/Libtensorflow/Lib/Types.pm
@@ -6,8 +6,11 @@ use Type::Library 0.008 -base,
 		TFTensor
 		TFGraph
 		TFDataType
+
+		Dims
 	)];
 use Type::Utils -all;
+use Types::Standard qw(ArrayRef Int);
 
 =type TFTensor
 
@@ -29,5 +32,13 @@ Type for class L<AI::TensorFlow::Libtensorflow::DataType>
 
 =cut
 class_type TFDataType => { class => 'AI::TensorFlow::Libtensorflow::DataType' };
+
+=type Dims
+
+C<ArrayRef> of C<Int>
+
+=cut
+declare Dims => as ArrayRef[Int];
+
 
 1;
